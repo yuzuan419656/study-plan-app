bd4fc739140bc9ef0e22034b96c073d510ae0a8b
Kotaro <yuzuan419656@gmail.com>
2026-08-08 15:51:32 +0900

    削除機能を追加しました。

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Path, HTTPException
+from fastapi import FastAPI, status, Path, HTTPException, Response
 
 from app.schemas import TaskCreate, TaskResponse
 
@@ -10,7 +10,7 @@ app = FastAPI(
 
 
 tasks: list[TaskResponse] = []
-
+next_task_id: int = 1
 
 
 def find_task_index(task_id: int) -> int:
@@ -54,12 +54,15 @@ def get_task(
         response_model=TaskResponse, 
         status_code=status.HTTP_201_CREATED)
 def create_task(task: TaskCreate):
+    global next_task_id
+
     new_task = TaskResponse(
-        id=len(tasks) + 1,
+        id=next_task_id,
         **task.model_dump(),
     )
 
     tasks.append(new_task)
+    next_task_id += 1
     return new_task
 
 
@@ -77,3 +80,17 @@ def update_task(
 
     tasks[task_index] = task
     return task
+
+
+@app.delete(
+    "/tasks/{task_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def delete_task(
+    task_id: int = Path(gt=0)
+):
+    task_index = find_task_index(task_id)
+    tasks.pop(task_index)
+
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
